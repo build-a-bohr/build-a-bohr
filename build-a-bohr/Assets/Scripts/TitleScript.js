fb977869f99c6812f5fe35179765c70cf2362534
vet12345w <eli.verbrugge@gmail.com>
2015-03-28 16:34:28 -0500

    Scene Progression with mouse
    Added scene progression when user clicks on left mouse button..

--- build-a-bohr/Assets/Scripts/TitleScript.js
+++ build-a-bohr/Assets/Scripts/TitleScript.js
@@ -3,13 +3,16 @@
 // allow click to continue also
 
 function Start() {
-	Invoke("Load", 3);
+	//Invoke("Load", 3);
 }
 
 function Update() {
+	if(Input.GetMouseButtonDown(0)){
+			Application.LoadLevel(Application.loadedLevel + 1);
+	}	
 	
 }
 
 function Load() {
-	Application.LoadLevel(1);
+	//Application.LoadLevel(1);
 }
