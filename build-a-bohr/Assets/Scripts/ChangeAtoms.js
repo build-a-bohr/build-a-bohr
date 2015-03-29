6758701399096520cc6445e7daed62aa20f0588f
vet12345w <eli.verbrugge@gmail.com>
2015-03-29 14:08:14 -0500

    Some scripts now have comments!
    Defined what some of my scripts do so I don't harm the cat...... Wait
    what?

--- build-a-bohr/Assets/Scripts/ChangeAtoms.js
+++ build-a-bohr/Assets/Scripts/ChangeAtoms.js
@@ -1,7 +1,8 @@
 ﻿#pragma strict
-
+// global var that is used to tell atom scene what atom config to create, need more variables for protons & neutrons
 public static var atoms = 0;
 
+// If input (now its a button later it will be a user prompt, then set the global var equal to something (later user defined)
 function NumberOfAtoms(){
  atoms += 3;
  Debug.Log("hello");
