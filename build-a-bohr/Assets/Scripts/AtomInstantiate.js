6077290701975d8b5e05789f7f2a3889244e6c67
Aaron Raimist <aaronraimist@protonmail.ch>
2015-03-29 14:12:45 -0500

    get styled son

--- build-a-bohr/Assets/Scripts/AtomInstantiate.js
+++ build-a-bohr/Assets/Scripts/AtomInstantiate.js
@@ -1,14 +1,16 @@
 ﻿#pragma strict
+
 // placeholder variable as to not overwrite the global one
 var numberAtom = 0;
 
 function Start() {
 	
 }
+
 // sets the placeholder equal to global and if condition is met does something involving atoms, and no cats :(
 function Update() {
 	numberAtom = ChangeAtoms.atoms;
- 	if(numberAtom == 3){
+ 	if(numberAtom == 3) {
 		gameObject.SetActive(false);
 	}
 }
--- build-a-bohr/Assets/Scripts/ChangeAtoms.js
+++ build-a-bohr/Assets/Scripts/ChangeAtoms.js
@@ -1,9 +1,10 @@
 ﻿#pragma strict
+
 // global var that is used to tell atom scene what atom config to create, need more variables for protons & neutrons
 public static var atoms = 0;
 
 // If input (now its a button later it will be a user prompt, then set the global var equal to something (later user defined)
 function NumberOfAtoms(){
- atoms += 3;
- Debug.Log("hello");
+	atoms += 3;
+	//Debug.Log("hello");
 }
