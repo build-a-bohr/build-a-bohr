9932eb597310f3e9645597b949e142cbfe71a8d0
vet12345w <eli.verbrugge@gmail.com>
2015-03-29 21:11:50 -0500

    Because my chemistry knowledge is bad
    Renamed and resprited electrons to protons and randomized electron
    positions, also fixed that the inputted number now actually is the
    number of electrons to create.

--- build-a-bohr/Assets/Scripts/ChangeAtoms.js
+++ build-a-bohr/Assets/Scripts/ChangeAtoms.js
@@ -1,11 +1,9 @@
 ﻿#pragma strict
 
-// global var that is used to tell atom scene what atom config to create, need more variables for protons & neutrons
-public static var electrons = 0;
+// global var that is used to tell the instantiater how many electrons to create
+public static var protons = 0.0;
 
-// If input (now its a button later it will be a user prompt, then set the global var equal to something (later user defined)
-function NumberOfElectrons(howmany : String){
-	 electrons = parseInt (howmany);
-	 Debug.Log(electrons + 1);
-	//Debug.Log("hello");
+// Convert user input to a number
+function NumberOfProtons(howmany : String){
+	 protons = parseInt (howmany);
 }
