eb57d7bbc64cc7257a630e60b3f20c5e3d9f002f
Aaron Raimist <aaronraimist@protonmail.ch>
2015-03-30 22:19:15 -0500

    primitive message system

---
+++ build-a-bohr/Assets/Scripts/MessageScript.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var MessageText : UnityEngine.UI.Text;
+
+var MessageArr = ["this is message 1", "this is message 2"]; // primitive message system
+var i = 0;
+
+function Start() {
+
+}
+
+function Update() {
+	if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
+		NewMessage();	
+	}
+}
+
+function NewMessage() {
+	MessageText.text = MessageArr[i];
+	i++;
+}
--- build-a-bohr/Assets/Scripts/SceneChanger.js
+++ build-a-bohr/Assets/Scripts/SceneChanger.js
@@ -5,6 +5,7 @@ function Start() {
 }
 
 function Update() {
+
 }
 
 function Load() {
