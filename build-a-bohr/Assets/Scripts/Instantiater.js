371fde6ee754f07069c26f1a5ead6d5862641f0e
vet12345w <eli.verbrugge@gmail.com>
2015-03-31 18:23:58 -0500

    Tweaks to particles and removing errors

--- build-a-bohr/Assets/Scripts/Instantiater.js
+++ build-a-bohr/Assets/Scripts/Instantiater.js
@@ -3,6 +3,7 @@
 var TOTAL_NUM_ELEMENTS = 118; // const?
 
 var protons : Transform;
+var neutrons : Transform;
 var numProtons = ChangeAtoms.protons;
 var prevNumProtons = 0;
 var range = 0;
@@ -16,6 +17,7 @@ function Update() {
 		Changer();
 	} else if(numProtons > TOTAL_NUM_ELEMENTS) {
 		Destroyer("proton");
+		
 	}
 }
 
@@ -26,13 +28,13 @@ function Changer() {
 		range = 0.75;
 	}
 	if(numProtons > 45 && numProtons < 93) {
-		range = 2.00;
+		range = 1.50;
 	}
 	if(numProtons > 93 && numProtons <= 118) {
-		range = 3.00;
+		range = 1.75;
 	}
 	
-	Destroyer("proton");
+	//Destroyer("proton");
 	
 	if(numProtons == 0) {
 		numProtons = ChangeAtoms.protons;
@@ -51,16 +53,15 @@ function Creator() {
 	Debug.Log("Creator()"); // check if actually happening
 
 	for(var y = 0; y < numProtons; y++) {
-		var pos = Vector3(Random.Range(0.0, range + 1), Random.Range(0.0, range + 1), 0);
+		var pos = Vector3(Random.Range(0.0, range + 1), Random.Range(0.0, range + 1), range);
 		Instantiate(protons, pos, Quaternion.identity);
 	}
+	
 }
 
 function Destroyer(particle) {
 	var gos = GameObject.FindGameObjectsWithTag(particle);
-	
 	if(gos.Length > 0) {
-		for(var i = 0; i < gos.Length; i++) {
 			Destroy(gos[i]);
 		}
 		
