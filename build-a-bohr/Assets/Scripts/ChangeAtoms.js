cdbd7eb9973c504f46f1f4414fc7849692060a4f
Aaron Raimist <aaronraimist@protonmail.ch>
2015-03-30 21:56:14 -0500

    some changes to Instantiater, playing with range

--- build-a-bohr/Assets/Scripts/ChangeAtoms.js
+++ build-a-bohr/Assets/Scripts/ChangeAtoms.js
@@ -6,12 +6,17 @@ public static var neutrons = 0.0;
 
 // Convert user input to a number
 function NumberOfProtons(howmanyP : String) {
-	 protons = parseInt(howmanyP);
+	try {
+		protons = parseInt(howmanyP);
+	} catch(e) {
+		Debug.Log("Empty string passed to parseInt in function NumberOfProtons()");
+	}
 }
 
 function NumberOfNeutrons(howmanyN : String) {
+	try {
      neutrons = parseInt(howmanyN);
+	} catch(e) {
+		Debug.Log("Empty string passed to parseInt in function NumberOfNeutrons()");
+	}
 }
-
-
-// ^ functions must check if nil to avoid error
--- build-a-bohr/Assets/Scripts/Instantiater.js
+++ build-a-bohr/Assets/Scripts/Instantiater.js
@@ -1,61 +1,70 @@
 ﻿#pragma strict
 
+var TOTAL_NUM_ELEMENTS = 118; // const?
+
 var protons : Transform;
-var number = ChangeAtoms.protons;
-var prevnumber = -1;
+var numProtons = ChangeAtoms.protons;
+var prevNumProtons = 0;
 var range = 0;
 
 function Update() {
-	number = ChangeAtoms.protons;
+	numProtons = ChangeAtoms.protons;
 
-	if(prevnumber != number) {	
-		Debug.Log(number);
+	if(numProtons != prevNumProtons && numProtons != 0 && numProtons <= TOTAL_NUM_ELEMENTS) {
+		Debug.Log("numProtons: " + numProtons);
 
-		Change();
+		Changer();
+	} else if(numProtons > TOTAL_NUM_ELEMENTS) {
+		Destroyer("proton");
 	}
 }
 
-function Change() {
-	Debug.Log("Change()");
+function Changer() {
+	Debug.Log("Changer()");
 
-	if(number > 0 && number <= 45) {
-		range = 0.50;
+	if(numProtons > 0 && numProtons <= 45) {
+		range = 0.75;
 	}
-	if(number > 45 && number < 93) {
+	if(numProtons > 45 && numProtons < 93) {
+		range = 2.00;
+	}
+	if(numProtons > 93 && numProtons <= 118) {
 		range = 3.00;
 	}
-	if(number > 93 && number <= 118) {
-		range = 4.00;
+	
+	Destroyer("proton");
+	
+	if(numProtons == 0) {
+		numProtons = ChangeAtoms.protons;
 	}
 	
-	if(Destroy()) {
-		Debug.Log("Destroy");
-	}
-	
-	if(number == 0) {
-		number = ChangeAtoms.protons;
-	}
-	
-	if(number > 0 && number <= 118) {			
-		Debug.Log("Create"); // check if actually happening
-		Create();
-		prevnumber = ChangeAtoms.protons;
+	if(numProtons > 0 && numProtons <= TOTAL_NUM_ELEMENTS) {			
+		Creator();
+		prevNumProtons = ChangeAtoms.protons;
 	}
 }
 
-function Create() {
-	for(var y = 0; y < number; y++) {
-		Instantiate(protons, Vector3 (Random.Range(0.0, range + 1), Random.Range(0.0, range + 1), 0), Quaternion.identity);
+// allow for type of particle to be passed in
+// try to eliminate blank spaces between protons, instantiate objects touching other objects
+// spawn in circle
+function Creator() {
+	Debug.Log("Creator()"); // check if actually happening
+
+	for(var y = 0; y < numProtons; y++) {
+		var pos = Vector3(Random.Range(0.0, range + 1), Random.Range(0.0, range + 1), 0);
+		Instantiate(protons, pos, Quaternion.identity);
 	}
 }
 
-function Destroy() {
-	var gos = GameObject.FindGameObjectsWithTag("proton");
+function Destroyer(particle) {
+	var gos = GameObject.FindGameObjectsWithTag(particle);
 	
 	if(gos.Length > 0) {
 		for(var i = 0; i < gos.Length; i++) {
 			Destroy(gos[i]);
 		}
+		
+		Debug.Log("Destroyer()");
 		return true;
 	} else {
 		return false;
