0642dc720eeb8a2c9c26a741ada37e9494b74544
vet12345w <eli.verbrugge@gmail.com>
2015-04-07 18:20:25 -0500

    Added Electron shells
    Nothing beyond 10 electrons and no actual electrons

---
+++ build-a-bohr/Assets/Scripts/ElectronController.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+public static var electrons = 0;
+var protons = ChangeAtoms.protons;
+var electronSprite : Transform;
+var BohrShell : Transform;
+
+function Start () {
+ electrons = protons;
+}
+function Update (){
+
+if(electrons > 0){
+  Create ();
+ }
+
+}
+function Create () {
+ if(electrons > 0){
+  var pos = Vector3(0, 0, 0);
+  Instantiate(BohrShell, pos, Quaternion.identity);
+  BohrShell.transform.localScale += new Vector2(0.4F, 0.4F);
+  electrons -= 2;
+  if(electrons > 0){
+  	Instantiate(BohrShell, pos, Quaternion.identity);
+  	BohrShell.transform.localScale += new Vector2(0.4F, 0.4F);
+  	electrons -= 8;
+  }
+ }
+}
