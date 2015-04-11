af6410efdfdc73fd35c46d75c352a54a6693598f
vet12345w <eli.verbrugge@gmail.com>
2015-04-10 20:33:06 -0500

    Temporary array for elements, improved electrons
    Electron rings now work for up to 2 electrons!

--- build-a-bohr/Assets/Scripts/ElectronController.js
+++ build-a-bohr/Assets/Scripts/ElectronController.js
@@ -4,12 +4,15 @@ public static var electrons = 0;
 var protons = ChangeAtoms.protons;
 var electronSprite : Transform;
 var BohrShell : Transform;
+var n = 1;
+var shift = 0;
+var electronsToCount = 0;
 
 function Start () {
  electrons = protons;
 }
 function Update (){
-
+Debug.Log(Mathf.Acos(Mathf.PI/4));
 if(electrons > 0){
   Create ();
  }
@@ -17,14 +20,24 @@ if(electrons > 0){
 }
 function Create () {
  if(electrons > 0){
-  var pos = Vector3(0, 0, 0);
-  Instantiate(BohrShell, pos, Quaternion.identity);
-  BohrShell.transform.localScale += new Vector2(0.4F, 0.4F);
-  electrons -= 2;
-  if(electrons > 0){
+  	var pos = Vector3(0, 0, 1);
   	Instantiate(BohrShell, pos, Quaternion.identity);
-  	BohrShell.transform.localScale += new Vector2(0.4F, 0.4F);
-  	electrons -= 8;
-  }
+  	if(electrons >= 2){
+  	 electronsToCount = 2;
+  	}
+  	if(electrons < 2){
+  	 electronsToCount = electrons;
+  	}
+  	// use trig to spawn 2 electrons around the cricle
+  	for(var y = electronsToCount; y > 0; y -= 1){
+  	 Instantiate(electronSprite, Vector3((n * Mathf.Acos(0)), 0, 0), Quaternion.identity);
+  	 n = -1;
+  	}
+  electrons -= 2;
+  //if(electrons > 0){
+  	//Instantiate(BohrShell, pos, Quaternion.identity);
+  	//BohrShell.transform.localScale += new Vector2(0.2F, 0.2F);
+  //	electrons -= 8;
+  //}
  }
 }
