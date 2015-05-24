0f2a4a838d6e2068094597ce0843752c980e0993
vet12345w <eli.verbrugge@gmail.com>
2015-05-23 19:58:36 -0500

    More stuff
    yay!

---
+++ build-a-bohr/Assets/Scripts/Namesetter.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+var Nname = ElementList.NameArray[protons];
+var protons = ChangeAtoms.protons;
+var neutrons = ChangeAtoms.protons;
+function Start () {
+
+}
+
+function Update () {
+    protons = ChangeAtoms.protons;
+    Nname = ElementList.NameArray[protons];
+	GetComponent.<UnityEngine.UI.Text>().text = Nname;
+}
